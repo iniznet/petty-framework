af4ee51680fecd3471d60a0b3cfb26e7a3a43072
niznet <iniznet@gmail.com>
2023-05-11 14:48:15 +0700

    feat: dotenv

--- src/Petty/Petty.php
+++ src/Petty/Petty.php
@@ -8,6 +8,14 @@ class Petty
 {
 	public function boot(): void
 	{
+		$this->loadEnvironmentVariables();
 		(new Route)::run();
+	}
+
+	private function loadEnvironmentVariables(): void
+	{
+		$dotenv = \Dotenv\Dotenv::createImmutable(BASE_PATH);
+		$dotenv->load();
+		$dotenv->required(['DB_HOST', 'DB_NAME', 'DB_USER', 'DB_PASS']);
 	}
 }
