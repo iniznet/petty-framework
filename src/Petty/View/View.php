9d7788feeebd004ffb97406ebe586730325b4721
niznet <iniznet@gmail.com>
2023-05-11 14:47:51 +0700

    feat: view

---
+++ src/Petty/View/View.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Petty\View;
+
+class View
+{
+	protected static string $basePath = RESOURCES_PATH . '/views';
+
+	public static function render(string $view, array $data = []): void
+	{
+		$view = self::$basePath . '/' . $view . '.php';
+
+		if (!file_exists($view)) {
+			throw new \Exception('View not found');
+		}
+
+		ob_start();
+		extract($data);
+		include_once $view;
+		$content = ob_get_clean();
+		ob_end_clean();
+
+		echo $content ?: '';
+	}
+
+	public function __invoke(string $view, array $data = []): void
+	{
+		self::render($view, $data);
+	}
+}
