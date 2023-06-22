f8f6b0c2ece55840dcf7ef3b33ef0e02288e0407
niznet <iniznet@gmail.com>
2023-06-23 02:54:40 +0700

    refactor(model): escape table name

--- src/Petty/Database/Model.php
+++ src/Petty/Database/Model.php
@@ -20,7 +20,7 @@ class Model
     public static function all(): array
     {
         $table = (new static)->table;
-        $query = "SELECT * FROM {$table}";
+        $query = "SELECT * FROM `{$table}`";
         $statement = (new static)->statement->prepare($query);
         $statement->execute();
         return $statement->fetchAll(PDO::FETCH_OBJ);
@@ -30,7 +30,7 @@ class Model
     {
         $table = (new static)->table;
         $primaryKey = (new static)->primaryKey;
-        $query = "SELECT * FROM {$table} WHERE {$primaryKey} = :id";
+        $query = "SELECT * FROM `{$table}` WHERE {$primaryKey} = :id";
         $statement = (new static)->statement->prepare($query);
         $statement->bindParam(':id', $id);
         $statement->execute();
@@ -39,7 +39,7 @@ class Model
 
     public function findWhere(array $data)
     {
-        $query = "SELECT * FROM {$this->table} WHERE ";
+        $query = "SELECT * FROM `{$this->table}` WHERE ";
         $query .= implode(' AND ', array_map(function ($key) {
             return $key . ' = :' . $key;
         }, array_keys($data)));
@@ -53,7 +53,7 @@ class Model
 
     public function row(): int
     {
-        $query = "SELECT * FROM {$this->table}";
+        $query = "SELECT * FROM `{$this->table}`";
         $statement = $this->statement->prepare($query);
         $statement->execute();
         return $statement->rowCount();
@@ -61,7 +61,7 @@ class Model
 
     public function create(array $data): bool
     {
-        $query = "INSERT INTO {$this->table} SET ";
+        $query = "INSERT INTO `{$this->table}` SET ";
         $query .= implode(', ', array_map(function ($key) {
             return $key . ' = :' . $key;
         }, array_keys($data)));
@@ -74,7 +74,7 @@ class Model
 
     public function update(int $id, array $data): bool
     {
-        $query = "UPDATE {$this->table} SET ";
+        $query = "UPDATE `{$this->table}` SET ";
         $query .= implode(', ', array_map(function ($key) {
             return $key . ' = :' . $key;
         }, array_keys($data)));
@@ -89,7 +89,7 @@ class Model
 
     public function delete(int $id): bool
     {
-        $query = "DELETE FROM {$this->table} WHERE {$this->primaryKey} = :id";
+        $query = "DELETE FROM `{$this->table}` WHERE {$this->primaryKey} = :id";
         $statement = $this->statement->prepare($query);
         $statement->bindValue(':id', $id);
         return $statement->execute();
